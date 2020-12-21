448bba15e85fccc76386d2204ddeb6fc1892750a
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2020-12-21 17:37:10 -0400

    Criação das telas de listagem e detalhes do produto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myshop_flutter/utils/app_routes.dart';
+import 'package:myshop_flutter/views/product_detail_screen.dart';
+import 'package:myshop_flutter/views/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,25 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Minha Loja',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Minha Loja'),
-      ),
-      body: Center(
-        child: Text(''),
-      ),
+      home: ProductsOverviewScreen(),
+      routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen()},
     );
   }
 }
--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:myshop_flutter/data/dummy_data.dart';
+import 'package:myshop_flutter/models/product.dart';
+import 'package:myshop_flutter/widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: loadedProducts.length,
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
+      ),
+    );
   }
 }
